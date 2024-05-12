ebd824d46d3243eb7f90c0d16e262cdf87c411a9
Pedro Prieto <pedpral@gmail.com>
2024-05-12 20:48:43 +0200

    Amplify config in Vue

--- src/main.js
+++ src/main.js
@@ -5,16 +5,21 @@
  */
 
 // Plugins
-import { registerPlugins } from '@/plugins'
+import { registerPlugins } from "@/plugins";
 
 // Components
-import App from './App.vue'
+import App from "./App.vue";
 
 // Composables
-import { createApp } from 'vue'
+import { createApp } from "vue";
 
-const app = createApp(App)
+import { Amplify } from "aws-amplify";
+import outputs from "../amplify_outputs.json";
 
-registerPlugins(app)
+Amplify.configure(outputs);
 
-app.mount('#app')
+const app = createApp(App);
+
+registerPlugins(app);
+
+app.mount("#app");
