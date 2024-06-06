4ccb633d357ae6cc911a3586f1034360d24680a8
Pedro Prieto <pedpral@gmail.com>
2024-06-06 10:51:28 +0200

    Activo histórico

--- src/views/ActivoHistorico.vue
+++ src/views/ActivoHistorico.vue
@@ -1,9 +1,17 @@
 <template>
-  <div>histórico</div>
-
-  <div v-for="sala in store.currentItem.salas">
-    {{ store.currentItem.salaId }}
-  </div>
+  <v-list lines="two">
+    <v-list-item
+      v-for="sala in store.currentItem.salas.slice().reverse()"
+      :key="sala.id"
+    >
+      <v-list-item-title>
+        {{ store.getNombreSalaById(sala.salaId) }}
+      </v-list-item-title>
+      <v-list-item-subtitle>
+        {{ new Date(sala.createdAt).toLocaleString() }}
+      </v-list-item-subtitle>
+    </v-list-item>
+  </v-list>
 </template>
 
 <script setup>
